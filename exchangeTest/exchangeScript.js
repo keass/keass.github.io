4719c39ea20e39a824ac187ceeb1284b7f0d2ae6
keass <keass9@gmail.com>
2014-11-18 03:40:06 +0900

    금액 입력시 상대적으로 바뀌도록 동작

--- exchangeTest/exchangeScript.js
+++ exchangeTest/exchangeScript.js
@@ -3,12 +3,16 @@
  // 생략 0. 인풋 2개 있는지 검증, 나라 선택 검증 > 없으면 생성, 나라 기본으로
  1. 변경 사항은 실시간으로 적용, 단위표기, 나라 지도 등
  // 1. 셀렉트시 지도 변경, 단위 하단, 단위 우측 변경
- 2. 금액 적을때, 하단에 실시간 변경 상대적으로 둘다
+ // 2. 금액 적을때, 하단에 실시간 변경 상대적으로 둘다
  2. 금액 적고, 국가 바꿀때도 수치는 그대로 유지, 새로 계산
+ 3. 숫자만 입력 가능하도록
+ 4. 자리수 표시하기
+
+ // 하단 내용
  3. 하단에 매매기준율 전일대비 등락율 값 가져오기
  4. 오름은 빨강, 내림은 파랑
-
  5. 가능하면 국가별 환율 넣고, 국가 기준 금액 넣어서 새로고침
+
  */
 var nhn = {};
 
@@ -44,19 +48,26 @@ nhn.exchange.prototype = {
         this.selectNation02.addEventListener("change",function(){
             that.changeNationNxt();
         });
+        this.currencyInput01.addEventListener("keydown",function(){
+            that.changeNationPre();
+        });
+        this.currencyInput02.addEventListener("keydown",function(){
+            that.changeNationNxt();
+        });
     },
     changeNationPre:function(){
         this.stateNationPre = this.selectNation01.options[this.selectNation01.selectedIndex].getAttribute('data-unit');
-        console.log(this.stateNationPre);
         this.currencyInput02.value = this.currnetCalc(this.stateNationPre, this.stateNationNxt, this.currencyInput01.value);
         this.changeNationFlagPre();
         this.changeNationUnitPre();
+        this.changeNationUnitNxt();
     },
     changeNationNxt:function(){
         this.stateNationNxt = this.selectNation02.options[this.selectNation02.selectedIndex].getAttribute('data-unit');
-        this.currencyInput02.value = this.currnetCalc(this.stateNationNxt, this.stateNationPre, this.currencyInput01.value);
+        this.currencyInput01.value = this.currnetCalc(this.stateNationNxt, this.stateNationPre, this.currencyInput02.value);
         this.changeNationFlagNxt();
         this.changeNationUnitNxt();
+        this.changeNationUnitPre();
     },
     changeNationUnitPre:function(){
         this.unitLeft[0].innerHTML = this.stateNationPre;
@@ -72,8 +83,8 @@ nhn.exchange.prototype = {
     changeNationFlagNxt:function() {
         this.flag[1].setAttribute("class","flag "+(this.stateNationNxt).toLowerCase());
     },
-    currnetCalc : function(nation1,nation2,exchValue){
-        return (exchValue)/(this.currencyData[arguments[0]])*(this.currencyData[arguments[1]]);
+    currnetCalc : function(){
+        return arguments[2]/(this.currencyData[arguments[0]])*(this.currencyData[arguments[1]]);
     }
 };
 
